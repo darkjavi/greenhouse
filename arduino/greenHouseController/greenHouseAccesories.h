c6709abf41e47c92be8b82f4d0f50cc95a819e87
darkjavi <darkjavi@gmail.com>
2013-12-23 16:56:17 +0100

    new file:   arduino/greenHouseController/greenHouseAccesories.h 	modified:   arduino/greenHouseController/greenHouseController.ino

---
+++ arduino/greenHouseController/greenHouseAccesories.h
@@ -0,0 +1,215 @@
+#include <StandardCplusplus.h>
+#include <serstream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include </home/darkjavi/sketchbook/libraries/StandardCplusplus/StandardCplusplus.h>
+#include </home/darkjavi/sketchbook/libraries/StandardCplusplus/serstream>
+#include </home/darkjavi/sketchbook/libraries/StandardCplusplus/string>
+#include </home/darkjavi/sketchbook/libraries/StandardCplusplus/vector>
+#include </home/darkjavi/sketchbook/libraries/StandardCplusplus/iterator>
+
+#include <math.h>
+class led{
+public:
+    led(int pin, int watts = 10) : m_pin(pin) , m_watts(watts) {}
+    void switchOn()  {digitalWrite(m_pin, HIGH);m_state=true;}
+    void switchOff() {digitalWrite(m_pin, LOW);m_state=false;}
+    int  watts()     {return m_watts;}
+    bool isOn()      {return m_state;}
+private:
+    bool   m_state;
+    int    m_pin;
+    int    m_watts;
+};
+
+class tempSensor{
+public:
+    tempSensor(int sensorPin = -1)
+    {
+        m_sensorPin = sensorPin;
+        m_avgTemp = 30;
+    }
+	double thermister(int RawADC) 
+	{
+		double Temp;
+		Temp = log(((10240000/RawADC) - 10000));
+		Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
+		Temp = Temp - 273.15;            // Convert Kelvin to Celcius
+		//Temp = (Temp * 9.0)/ 5.0 + 32.0; // Convert Celcius to Fahrenheit
+		return Temp;
+	}
+    int read()
+    {
+		int value = analogRead(m_sensorPin);
+		double temp = thermister(value);
+		int tempInt;
+		tempInt = temp;
+                m_avgTemp = ((m_avgTemp *9)+temp)/10;
+	#ifdef DEBUG
+		cout << "[DEBUG] Temp pin value:"<< value ;
+		cout << " Estimated temp:"<< temp;
+		cout << " Average temp:"<< m_avgTemp <<"\n";
+	#endif
+		return temp;
+	}
+    bool isValid()  
+    {
+      if(m_sensorPin<0)
+        return false;
+      if(isnan(read()))
+        return false;
+      if(read() < TEMP_SENSOR_MIN_TEMP)
+        return false;
+      return true;
+    }
+private:
+    int m_sensorPin;
+    double m_avgTemp;
+};
+
+class fan{
+public:
+    fan(int mosfetPin=-1) : m_pin(mosfetPin) {}
+    bool isValid()  {return m_pin>=0;}
+    void setSpeed(int speed = 255) 
+    {
+      analogWrite(m_pin,speed);
+      m_speed = speed;
+    }
+    int  speed()	{return m_speed;}
+private:
+    int m_pin;
+    int m_speed;
+};
+
+class pump
+{
+public:
+    pump(int pump_pin = -1, int flow_pin_int = -1)
+    {
+        m_pumpPin = pump_pin;
+        m_flowPin = flow_pin_int;
+    }
+
+    void flow()
+    {
+        cout << "[DEBUG]Something flew!\n";
+    }
+
+private:
+    int m_pumpPin;
+    int m_flowPin;
+};
+
+class lamp{
+public:
+    lamp(int fanPin = -1, int tempPin = -1)
+    {
+        m_fan = fan(fanPin);
+        m_tempSensor = tempSensor(tempPin);
+    }
+    ~lamp(){switchOff();}
+    vector<led>& leds()                     {return m_leds;}
+    bool         haveFan()                  {return m_fan.isValid();}
+    bool         haveTempSensor()           {return m_tempSensor.isValid();}
+    void         addLed(int pin, int watts) 
+    {
+		m_leds.push_back(led(pin,watts));
+	#ifdef DEBUG
+		cout << "[DEBUG] Added led on pin:"<<pin<<" - " << watts << "watts\n";
+	#endif
+    }
+
+    void switchOn()
+    {
+       m_fan.setSpeed(128);
+       for(int i = 0 ; i < m_leds.size() ; i++)
+        {
+            m_leds[i].switchOn();
+        #ifdef DEBUG
+            cout << "[DEBUG]Lamp power: " << activePower() << "W\n";
+        #endif
+            delay(100);
+        }
+    }
+    void switchOff()
+    {
+        for(int i = 0 ; i < m_leds.size() ; i++)
+        {
+            m_leds[i].switchOff();
+        #ifdef DEBUG
+            cout << "[DEBUG]Lamp power: " << activePower() << "W\n";
+        #endif
+            delay(50);
+        }
+    }
+    void test()
+    {
+        m_fan.setSpeed(255);
+        for(int i = 0 ; i<m_leds.size() ; i++)
+        {
+            m_leds[i].switchOn();
+            delay(200);
+            m_leds[i].switchOff();
+            m_fan.setSpeed(m_fan.speed()/2);
+        }
+            m_fan.setSpeed(0);
+    }
+    int activePower()
+    {
+        int power = 0;
+        for(int i = 0 ; i < m_leds.size() ; i++)
+        {
+            if(m_leds[i].isOn()) power += m_leds[i].watts();
+        }
+        return power;
+    }
+    int totalPower()
+    {
+        int power = 0;
+        for(int i = 0 ; i < m_leds.size() ; i++)
+        {
+            power += m_leds[i].watts();
+        }
+        return power;
+    }
+
+    void check()
+    {
+		if(m_tempSensor.isValid())
+		{
+			int currentTemp = m_tempSensor.read();
+			if(currentTemp < LAMP_MIN_TEMPERATURE)
+				m_fan.setSpeed(0);
+			else if (currentTemp > LAMP_CRITICAL_TEMP)
+			{
+				m_fan.setSpeed(255);
+				switchOff();
+			}
+			else if(currentTemp > LAMP_MIN_TEMPERATURE)
+                        {
+                          if(currentTemp > LAMP_TARGET_TEMPERATURE)
+                          {
+                            m_fan.setSpeed(200);
+                          }
+                          else
+                          {
+                            m_fan.setSpeed(128);
+                          }
+                        }
+		}
+		else
+		{//Si no hay sensor de temperatura ponemos le ventilador a tope
+			if(m_fan.isValid())
+			{
+				m_fan.setSpeed(255);
+			}
+		}
+    }
+
+private:
+    vector<led> m_leds;
+    fan         m_fan;
+    tempSensor  m_tempSensor;
+};
--- arduino/greenHouseController/greenHouseController.ino
+++ arduino/greenHouseController/greenHouseController.ino
@@ -3,33 +3,46 @@
 #include <string>
 #include <vector>
 #include <iterator>
+#include </home/darkjavi/sketchbook/libraries/StandardCplusplus/StandardCplusplus.h>
+#include </home/darkjavi/sketchbook/libraries/StandardCplusplus/serstream>
+#include </home/darkjavi/sketchbook/libraries/StandardCplusplus/string>
+#include </home/darkjavi/sketchbook/libraries/StandardCplusplus/vector>
+#include </home/darkjavi/sketchbook/libraries/StandardCplusplus/iterator>
 
-#define LED0_PIN    5
-#define LED0_WATTS  10
+#define DEBUG
 
-#define LED1_PIN    6
+#define LED0_PIN    4
+#define LED0_WATTS  30
+
+#define LED1_PIN    7
 #define LED1_WATTS  10
 
-#define LED2_PIN    7
+#define LED2_PIN    8
 #define LED2_WATTS  10
 
-#define LED3_PIN    8
+#define LED3_PIN    9
 #define LED3_WATTS  10
 
-#define LED4_PIN    9
-#define LED4_WATTS  10
+//#define LED4_PIN    9
+//#define LED4_WATTS  10
 
-#define LED5_PIN    10
-#define LED5_WATTS  10
+//#define LED5_PIN    10
+//#define LED5_WATTS  10
 
-#define LED6_PIN    11
-#define LED6_WATTS  10
+//#define LED6_PIN    11
+//#define LED6_WATTS  10
 
 //#define LED7_PIN    12
 //#define LED7_WATTS  10
 
-//#define LAMP_TEMP_PIN A1
-//#define LAMP_FAN_PIN  5
+#define LAMP_TEMP_PIN A0
+#define LAMP_FAN_PIN  5
+
+#define TEMP_SENSOR_MIN_TEMP    5        //minimal possible temp(less will be considered as misread!)
+#define LAMP_MIN_TEMPERATURE 	30 	// treshold to start fans
+#define LAMP_TARGET_TEMPERATURE 40  // target temperature
+#define LAMP_CRITICAL_TEMP   	70	// shutdown temperature
+
 
 #define PUMP_PIN        3
 #define PUMP_FLOW_PIN   2
@@ -42,190 +55,93 @@ namespace std
   ohserialstream cout(Serial);
 }
 
-class led{
-public:
-    led(int pin, int watts = 10) : m_pin(pin) , m_watts(watts) {}
-    void switchOn()  {digitalWrite(m_pin, HIGH);m_state=true;}
-    void switchOff() {digitalWrite(m_pin, LOW);m_state=false;}
-    int  watts()     {return m_watts;}
-    bool isOn()      {return m_state;}
-private:
-    bool   m_state;
-    int    m_pin;
-    int    m_watts;
-};
-class fan{
-public:
-    fan(int pin=-1) : m_pin(pin) {}
-private:
-    int m_pin;
-};
+#include "greenHouseAccesories.h"
 
-class pump
-{
-public:
-    pump(int pump_pin = -1, int flow_pin = -1)
-    {
-        m_pumpPin = pump_pin;
-        m_flowPin = flow_pin;
-    }
-
-    void flow()
-    {
-        cout << "[DEBUG]Something flew!";
-    }
-
-private:
-    int m_pumpPin;
-    int m_flowPin;
-};
-
-class lamp{
-public:
-    lamp()
-    {
-    #ifdef LED0_PIN
-        led l0(LED0_PIN,LED0_WATTS);
-        m_leds.push_back(l0);
-    #endif
-    #ifdef LED1_PIN
-        led l1(LED1_PIN,LED1_WATTS);
-        m_leds.push_back(l1);
-    #endif
-    #ifdef LED2_PIN
-        led l2(LED2_PIN,LED2_WATTS);
-        m_leds.push_back(l2);
-    #endif
-    #ifdef LED3_PIN
-        led l3(LED3_PIN,LED3_WATTS);
-        m_leds.push_back(l3);
-    #endif
-    #ifdef LED4_PIN
-        led l4(LED4_PIN,LED4_WATTS);
-        m_leds.push_back(l4);
-    #endif
-    #ifdef LED5_PIN
-        led l5(LED5_PIN,LED5_WATTS);
-        m_leds.push_back(l5);
-    #endif
-    #ifdef LED6_PIN
-        led l6(LED6_PIN,LED6_WATTS);
-        m_leds.push_back(l6);
-    #endif
-    #ifdef LED7_PIN
-        led l7(LED7_PIN,LED7_WATTS);
-        m_leds.push_back(l7);
-    #endif
-
-    #ifdef LAMP_FAN_PIN
-        m_has_fan = true;
-        //m_fan = fan(LAMP_FAN_PIN);
-    #else
-        m_has_fan = false;
-    #endif
-
-    #ifdef LAMP_TEMP_PIN
-        m_has_temp = true;
-    #else
-        m_has_temp = false;
-    #endif
-    }
-    vector<led>&   leds()  {return m_leds;}
-    bool        has_fan()  {return m_has_fan;}
-    bool has_tempSensor()  {return m_has_temp;}
-    void switchOn()
-    {
-        for(int i = 0 ; i < m_leds.size() ; i++)
-        {
-            m_leds[i].switchOn();
-            cout << "[INFO]Lamp power: <<" << activePower() << "\n";
-            delay(100);
-        }
-    }
-    void switchOff()
-    {
-        for(int i = 0 ; i < m_leds.size() ; i++)
-        {
-            m_leds[i].switchOff();
-            cout << "[INFO]Lamp power: <<" << activePower() << "\n";
-            delay(50);
-        }
-    }
-    void test()
-    {
-        for(int i = 0 ; i<m_leds.size() ; i++)
-        {
-            m_leds[i].switchOn();
-            delay(200);
-            m_leds[i].switchOff();
-        }
-    }
-    int activePower()
-    {
-        int power = 0;
-        for(int i = 0 ; i < m_leds.size() ; i++)
-        {
-            if(m_leds[i].isOn()) power += m_leds[i].watts();
-        }
-        return power;
-    }
-    int totalPower()
-    {
-        int power = 0;
-        for(int i = 0 ; i < m_leds.size() ; i++)
-        {
-            power += m_leds[i].watts();
-        }
-        return power;
-    }
-
-    void check()
-    {
-
-    }
-
-private:
-    vector<led> m_leds;
-    bool        m_has_fan;
-    fan         m_fan;
-    bool        m_has_temp;
-};
-
-lamp greenHouseLamp;
-pump greenHousePump;
+lamp ghLamp;
+pump ghPump;
+unsigned int ips = 0;
 
 void sendData()
 {
     cout << "[DATA]" <<
-            "LAMP_TOTALPOWER:"      << greenHouseLamp.totalPower()  <<
-            "LAMP_ACTIVEPOWER:"     << greenHouseLamp.activePower() <<
+//            "LAMP_TOTALPOWER:"      << ghLamp.totalPower()  <<
+            "LAMP_ACTIVEPOWER:"     << ghLamp.activePower() <<
+            "IPS:"  << ips <<
             "\n";
+            ips = 0;
 }
 
 void setup()
 {
+  Serial.begin(115200);
     cout << "[INFO]INIT!\n";
-    Serial.begin(115200);
-    greenHouseLamp = lamp();
-#ifdef PUMP_PIN
-    attachInterrupt(0,flow,RISING);
+    int fanPin = -1;
+    int tempPin = -1;
+#ifdef LAMP_TEMP_PIN
+    tempPin = LAMP_TEMP_PIN;          
+#endif
+#ifdef LAMP_FAN_PIN
+	fanPin = LAMP_FAN_PIN;
+#endif
+    ghLamp = lamp(fanPin,tempPin);
+#ifdef LED0_PIN
+	ghLamp.addLed(LED0_PIN,LED0_WATTS);   
+#endif
+#ifdef LED1_PIN
+    ghLamp.addLed(LED1_PIN,LED1_WATTS);   
+#endif    
+#ifdef LED2_PIN
+    ghLamp.addLed(LED2_PIN,LED2_WATTS);   
+#endif
+#ifdef LED3_PIN
+    ghLamp.addLed(LED3_PIN,LED3_WATTS);   
+#endif
+#ifdef LED4_PIN
+    ghLamp.addLed(LED4_PIN,LED4_WATTS);   
+#endif
+#ifdef LED5_PIN
+    ghLamp.addLed(LED5_PIN,LED5_WATTS);   
+#endif
+#ifdef LED6_PIN
+    ghLamp.addLed(LED6_PIN,LED6_WATTS);   
+#endif
+#ifdef LED7_PIN
+    ghLamp.addLed(LED7_PIN,LED7_WATTS);   
 #endif
 
-    cout << "[INFO]Lamp has "<<greenHouseLamp.leds().size() << " leds\n";
-    cout << "[INFO]Lamp has "<<greenHouseLamp.totalPower()<<"Watts\n";
-    if(greenHouseLamp.has_fan()) cout << "[INFO]Lamp has fan control on pin: \n";
+    cout << "[INFO]Lamp have "<<ghLamp.leds().size() << " leds\n";
+    cout << "[INFO]Lamp have "<<ghLamp.totalPower()<<"Watts\n";
+    if(ghLamp.haveFan()) cout << "[INFO]Lamp have fan control on pin:"<<fanPin<<"\n";
     else cout << "[INFO]Lamp DOESN'T have fan control!\n";
-    if(greenHouseLamp.has_tempSensor()) cout << "[INFO]Lamp has temp sensor on pin:\n";
+    if(ghLamp.haveTempSensor()) cout << "[INFO]Lamp have temp sensor on pin:"<<tempPin<<"\n";
     else cout << "[INFO]Lamp DOESN'T have temp sensor!\n";
-    greenHouseLamp.test();
-    greenHouseLamp.switchOn();
-    greenHouseLamp.switchOff();
+    ghLamp.test();
+    ghLamp.switchOn();
+//    delay(1000);
+//    ghLamp.switchOff();
+//    delay(1000);
+//    ghLamp.leds()[0].switchOn();
+//    attachInterrupt(0,int0,RISING);
+}
+
+void int0()
+{
+//  noInterrupts();
+    ips++;
+//  interrupts();
 }
 
 void loop()
 {
-    greenHouseLamp.check();
+    ghLamp.check();
     sendData();
     delay(1000);
+    char *msg;
+	while (Serial.available()) {
+		// lee el byte entrante:
+		int	incomingByte = Serial.read();
+		// dice lo que ha recibido:
+		Serial.print("He recibido: ");
+		Serial.println(incomingByte, DEC);
+	}
 }
-
